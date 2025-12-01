37506acfe09fdef0404949eebfe331d98ba552fa
nbarchi <nicobarrera.g10@gmail.com>
2025-11-30 19:19:39 -0500

    First Commit

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+  }
+
+}
---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [NgOptimizedImage],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css'
+})
+export class LandingComponent {
+
+}
+
